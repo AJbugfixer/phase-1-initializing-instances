574ad0e5ea5167e13fdba4dbdca4bf8355c78956
Ajay <ajay.ip3105@gmail.com>
2022-05-14 19:14:20 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // Write your code here
+
 class Breakfast{
     constructor (food,drink){
         this.food = food
         this.drink = drink
     }
-    
+
 }
 
 class Lunch{
@@ -16,10 +17,11 @@ class Lunch{
 }
 
 class Dinner{
+   // _dessert
     constructor (salad,soup,entree,dessert){
         this.salad = salad
         this.soup = soup
         this.entree = entree
-        this.dessert = dessert
+        this._dessert = dessert
     }
 }
